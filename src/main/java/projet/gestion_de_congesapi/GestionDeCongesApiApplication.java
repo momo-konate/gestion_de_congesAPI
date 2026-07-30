ea83bcc56405e7aa0005f8db9de233d1d143feef
momo_konate <mouhamedk996@gmail.com>
2026-07-30 00:33:40 +0000

    Creation de la structure du projet Spring Boot

---
+++ src/main/java/projet/gestion_de_congesapi/GestionDeCongesApiApplication.java
@@ -0,0 +1,13 @@
+package projet.gestion_de_congesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeCongesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDeCongesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projet/gestion_de_congesapi/GestionDeCongesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package projet.gestion_de_congesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeCongesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
